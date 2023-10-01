88779a1494e2727d11fc96d2ac9b19c10f6186de
matthias <matthias.mosthof@gmail.com>
2023-10-01 12:00:55 +0200

    uses routerview and seprates logic from mainlayout into appropiate comp

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,5 @@
 <template>
   <q-layout>
-    <OneSheet />
-    <AddSheet />
+    <router-view />
   </q-layout>
 </template>
-
-<script setup>
-import { onMounted } from "vue";
-import { useCheatSheetStore } from "stores/cheatsheetStore.js";
-import OneSheet from "../components/OneSheet.vue";
-import AddSheet from "../components/AddSheet.vue";
-const store = useCheatSheetStore();
-const { getLocalStorage } = store;
-
-onMounted(() => {
-  getLocalStorage();
-});
-</script>
