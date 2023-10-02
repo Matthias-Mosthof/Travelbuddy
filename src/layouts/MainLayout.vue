4d565175c3efd7327932fff1b4d0817ff157f669
matthias <matthias.mosthof@gmail.com>
2023-10-02 21:23:30 +0200

    adds NavBars to mainlayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,19 @@
 <template>
-  <q-layout>
-    <router-view />
+  <q-layout
+    view="hhr lpr ffr"
+    container
+    style="height: 100vh"
+    class="shadow-2 rounded-borders"
+  >
+    <NavBars />
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <router-view />
+      </q-page>
+    </q-page-container>
   </q-layout>
 </template>
+
+<script setup>
+import NavBars from "src/components/Nav/NavBars.vue";
+</script>
