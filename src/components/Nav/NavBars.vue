f629b3bd3f4818af181a0047cd8e9e9ac29ebed1
matthias <matthias.mosthof@gmail.com>
2023-10-08 23:09:48 +0200

    adds basic search input field

--- src/components/Nav/NavBars.vue
+++ src/components/Nav/NavBars.vue
@@ -1,10 +1,14 @@
+<script setup>
+import SearchInput from "../SearchInput.vue";
+</script>
+
 <template>
   <q-header reveal elevated bordered>
     <q-toolbar class="bg-blue-8 text-white">
       <q-btn flat round dense icon="assignment_ind" />
 
       <q-toolbar-title> Cheatsheet App </q-toolbar-title>
-
+      <SearchInput placeholder="Suche" />
       <q-btn flat round dense icon="apps" class="q-mr-xs" />
       <q-btn flat round dense icon="more_vert" />
     </q-toolbar>
