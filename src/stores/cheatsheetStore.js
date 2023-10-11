81091b110497cc9657fe54a63bf4f3b2f42b32d8
matthias <matthias.mosthof@gmail.com>
2023-10-11 21:56:14 +0200

    adds id flag inside document

--- src/stores/cheatsheetStore.js
+++ src/stores/cheatsheetStore.js
@@ -1,10 +1,14 @@
 import { defineStore } from "pinia";
 import {
+  deleteDoc,
+  doc,
   collection,
   addDoc,
   getDocs,
   serverTimestamp,
   updateDoc,
+  orderBy,
+  query,
 } from "firebase/firestore";
 import { db } from "src/Api/firebase";
 import { Notify } from "quasar";
@@ -17,8 +21,12 @@ export const useCheatSheetStore = defineStore("cheatSheets", {
   actions: {
     async addSheet(userInput) {
       try {
+        const id = this.createId();
         // note to self: addDoc function lets firerbase create an id, with setDoc function one can create own id
-        const docRef = await addDoc(collection(db, "sheets"), userInput);
+        const docRef = await addDoc(collection(db, "sheets"), {
+          ...userInput,
+          id,
+        });
         await updateDoc(docRef, {
           createdAt: serverTimestamp(),
         });
@@ -36,12 +44,24 @@ export const useCheatSheetStore = defineStore("cheatSheets", {
     },
 
     async fetchFirebaseDB() {
-      const data = await getDocs(collection(db, "sheets"));
+      let data = await getDocs(collection(db, "sheets"));
       const firebaseSheets = [];
       data.forEach((doc) => {
         firebaseSheets.push(doc.data());
       });
       this.sheets = firebaseSheets;
+    },
+
+    async removeSheet(id) {
+      try {
+        await deleteDoc(doc(db, "sheets", id));
+        console.log("successfully deleted document " + id);
+      } catch (error) {
+        console.log("tst");
+      }
+    },
+    createId() {
+      return Math.random(1, 10).toFixed(10).toString().replace(/^0\.?/, "");
     },
   },
 
