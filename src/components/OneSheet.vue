c12aa9ba67b00b51c992815d6c34237a63f104dd
matthias <matthias.mosthof@gmail.com>
2023-10-11 21:56:55 +0200

    adds clickable actions on each sheet

--- src/components/OneSheet.vue
+++ src/components/OneSheet.vue
@@ -5,12 +5,40 @@
         v-for="(entry, i) in state"
         :key="i"
         dark
+        flat
         bordered
         class="text-white my-card"
       >
         <q-card-section>
-          <div class="text-h6">
-            {{ entry.title }}
+          <div class="row items-center no-wrap">
+            <div class="col">
+              <div class="text-h6">
+                {{ entry.title }}
+              </div>
+              <div class="text-subtitle2">
+                {{ entry.createdAt }}
+              </div>
+            </div>
+
+            <div class="col-auto">
+              <q-btn color="grey-7" round flat icon="more_vert">
+                <q-menu cover auto-close>
+                  <q-list>
+                    <q-item
+                      clickable
+                      @click="triggerRemove(entry)"
+                      v-close-popup
+                    >
+                      <q-item-section>Remove Card</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item clickable v-close-popup>
+                      <q-item-section>Edit Card</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+            </div>
           </div>
         </q-card-section>
 
@@ -27,10 +55,17 @@ import { computed } from "vue";
 import { useCheatSheetStore } from "stores/cheatsheetStore.js";
 
 const store = useCheatSheetStore();
+const { removeSheet } = store;
 
 const state = computed(() => {
   return store.getCheatSheets;
 });
+
+function triggerRemove(entry) {
+  console.log(entry.id, "heeere");
+  removeSheet(docId);
+  console.log("remooove");
+}
 </script>
 
 <style scoped>
