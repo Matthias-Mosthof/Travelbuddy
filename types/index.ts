798ce597070dc84d429e129f7125ebb18f3e0b95
matthias <matthias.mosthof@gmail.com>
2023-11-28 21:58:01 +0100

    adds timestamp to interface

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,12 @@
+import type { Timestamp } from "firebase/firestore";
+
 export {};
 
 declare global {
   export interface NewPost {
     title: string;
     text: string;
+    createdAt: Timestamp;
   }
 
   export interface Post extends NewPost {
