a41bb4d62d270c15149258856287f8b3d107fa1e
matthias <matthias.mosthof@gmail.com>
2023-12-07 23:05:32 +0100

    adds basic admit layout

---
+++ layouts/admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout
+    class="shadow-2 rounded-borders"
+    container
+    style="height: 100vh"
+    view="hhr lpr ffr"
+  >
+    <NavBars />
+    <q-page-container>
+      <q-page
+        class="q-pa-md bg-secondary"
+        padding
+      >
+        <slot />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
