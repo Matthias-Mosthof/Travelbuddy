ccc6df8c73330182376b5d8c3028896e25fc6e23
matthias <matthias.mosthof@gmail.com>
2023-12-07 23:43:27 +0100

    applies eslint rules

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 definePageMeta({
-	layout: 'default',
+  layout: 'default',
 });
 
 const store = usePostsStore();
 const { fetchFirebaseDB } = store;
 
 onMounted(() => {
-	fetchFirebaseDB();
+  fetchFirebaseDB();
 });
 </script>
 
 <template>
-  <AllPosts />
-  <AddPost />
+  <q-page-container>
+    <PostsList />
+    <PostAdd />
+  </q-page-container>
 </template>
