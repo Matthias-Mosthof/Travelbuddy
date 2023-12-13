2bdabd81412ca1f78956f548410984b289076ffd
matthias <matthias.mosthof@gmail.com>
2023-12-13 21:54:56 +0100

    refactors postLists component template strucutre

--- components/AdminPostSettings.vue
+++ components/AdminPostSettings.vue
@@ -9,43 +9,58 @@ defineProps({
 </script>
 
 <template>
-  <q-menu
-    auto-close
-    cover
-  >
-    <q-list>
-      <q-item
-        v-close-popup
-        clickable
-        @click="usePostsStore().removePost(postId)"
+  <q-card-section>
+    <div class="row items-center no-wrap">
+      <div
+        class="col-auto"
       >
-        <q-item-section>Löschen</q-item-section>
-      </q-item>
-      <q-separator />
-      <q-item
-        v-close-popup
-        clickable
-      >
-        <q-item-section>Bearbeiten</q-item-section>
-      </q-item>
-      <q-separator />
+        <q-btn
+          color="grey-7"
+          flat
+          icon="more_vert"
+          round
+        >
+          <q-menu
+            auto-close
+            cover
+          >
+            <q-list>
+              <q-item
+                v-close-popup
+                clickable
+                @click="usePostsStore().removePost(postId)"
+              >
+                <q-item-section>Löschen</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item
+                v-close-popup
+                clickable
+              >
+                <q-item-section>Bearbeiten</q-item-section>
+              </q-item>
+              <q-separator />
 
-      <q-item
-        clickable
-        @click="usePostsStore().releasePost(postId)"
-      >
-        <q-item-section>
-          Freigeben
-        </q-item-section>
-      </q-item>
-      <q-item
-        clickable
-        @click="usePostsStore().rejectPost(postId)"
-      >
-        <q-item-section>
-          Ablehnen
-        </q-item-section>
-      </q-item>
-    </q-list>
-  </q-menu>
+              <q-item
+                clickable
+                @click="usePostsStore().releasePost(postId)"
+              >
+                <q-item-section>
+                  Freigeben
+                </q-item-section>
+              </q-item>
+              <q-item
+                clickable
+                @click="usePostsStore().rejectPost(postId)"
+              >
+                <q-item-section>
+                  Ablehnen
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </div>
+    </div>
+  </q-card-section>
 </template>
---
+++ components/PostContent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps({
+  postName: {
+    type: String,
+    default: '',
+  },
+  postAge: {
+    type: Number,
+    default: null,
+  },
+  postTitle: {
+    type: String,
+    default: '',
+  },
+  postText: {
+    type: String,
+    default: '',
+  },
+});
+
+</script>
+
+<template>
+  <q-card-section>
+    <div class="text-overline text-orange-9">
+      {{ `Von ${postName} (${postAge}) ` }}
+    </div>
+    <div class="text-h5 q-mt-sm q-mb-xs">
+      {{ postTitle }}
+    </div>
+    <div class="text-caption text-grey">
+      {{ postText }}
+    </div>
+  </q-card-section>
+</template>
