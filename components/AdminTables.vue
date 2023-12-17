4ebad2e36e0d9382af6c7debfcf6c92b7eeef12c
matthias <matthias.mosthof@gmail.com>
2023-12-17 22:55:27 +0100

    adds basic logic to reject and remove selection, buttons show dynamically and admin can choose what to do

--- components/AdminTables.vue
+++ components/AdminTables.vue
@@ -15,13 +15,23 @@ defineProps({
 });
 
 const selected = ref([] as Post[]);
-const openDialog = ref(false);
 const store = usePostsStore();
+
+const openReleaseDialog = ref(false);
+const openRejectDialog = ref(false);
+const openDeleteDialog = ref(false);
 
 function triggerReleasePosts() {
   selected.value.forEach((p: Post) => store.releasePost(p.id));
 }
 
+function triggerDeletePosts() {
+  selected.value.forEach((p: Post) => store.removePost(p.id));
+}
+
+function triggerRejectPosts() {
+  selected.value.forEach((p: Post) => store.rejectPost(p.id));
+}
 </script>
 
 <template>
@@ -68,17 +78,35 @@ function triggerReleasePosts() {
     :offset="[18, 18]"
     position="bottom-right"
   >
-    <q-btn
-      v-if="selected.length > 0"
-      color="red"
-      icon-right="check"
-      label="Speichern & Freigeben"
-      @click="openDialog = true"
-    />
+    <div class="q-gutter-md">
+      <q-btn
+        v-if="selected.length > 0"
+        color="green"
+        icon-right="check"
+        label="Auswahl Freigeben"
+        @click="openReleaseDialog = true"
+      />
+
+      <q-btn
+        v-if="selected.length > 0"
+        color="orange-8"
+        icon-right="delete"
+        label="Auswahl Löschen"
+        @click="openDeleteDialog = true"
+      />
+
+      <q-btn
+        v-if="selected.length > 0"
+        color="red"
+        icon-right="cancel"
+        label="Auswahl Ablehnen"
+        @click="openRejectDialog = true"
+      />
+    </div>
   </q-page-sticky>
 
   <q-dialog
-    v-model="openDialog"
+    v-model="openReleaseDialog"
     icon="check"
     persistent
   >
@@ -118,6 +146,100 @@ function triggerReleasePosts() {
             icon-right="check"
             label="Freigeben"
             @click="triggerReleasePosts"
+          />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-dialog>
+
+  <q-dialog
+    v-model="openRejectDialog"
+    icon="check"
+    persistent
+  >
+    <div>
+      <q-card
+        class="bg-red-6 text-white q-pa-md"
+        style="width: 400px"
+      >
+        <q-card-section>
+          <div class="text-h6">
+            Sind Sie sicher diese {{ selected.length }} Posts Abzulehnen?
+          </div>
+        </q-card-section>
+
+        <q-card-section
+          v-for="selectedPost in selected"
+          :key="selectedPost.id"
+          class="q-ma-xs q-pa-xs"
+        >
+          - "{{ selectedPost.title }}"" von "{{ selectedPost.name }}"
+        </q-card-section>
+
+        <q-card-actions
+          align="right"
+          class="bg-black text-teal"
+        >
+          <q-btn
+            v-close-popup
+            color="blue"
+            flat
+            label="Abbrechen"
+          />
+          <q-btn
+            v-close-popup
+            color="red"
+            flat
+            icon-right="check"
+            label="Ablehnen"
+            @click="triggerRejectPosts"
+          />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-dialog>
+
+  <q-dialog
+    v-model="openDeleteDialog"
+    icon="check"
+    persistent
+  >
+    <div>
+      <q-card
+        class="bg-red-6 text-white q-pa-md"
+        style="width: 400px"
+      >
+        <q-card-section>
+          <div class="text-h6">
+            Sind Sie sicher diese {{ selected.length }} Posts zu löschen?
+          </div>
+        </q-card-section>
+
+        <q-card-section
+          v-for="selectedPost in selected"
+          :key="selectedPost.id"
+          class="q-ma-xs q-pa-xs"
+        >
+          - "{{ selectedPost.title }}"" von "{{ selectedPost.name }}"
+        </q-card-section>
+
+        <q-card-actions
+          align="right"
+          class="bg-black text-teal"
+        >
+          <q-btn
+            v-close-popup
+            color="blue"
+            flat
+            label="Abbrechen"
+          />
+          <q-btn
+            v-close-popup
+            color="red"
+            flat
+            icon-right="check"
+            label="Löschen"
+            @click="triggerDeletePosts"
           />
         </q-card-actions>
       </q-card>
