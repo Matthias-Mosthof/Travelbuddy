4d722bba70089178cfc81fb3668777405a7df912
matthias <matthias.mosthof@gmail.com>
2023-12-17 22:36:47 +0100

    adds logic and styling to release selected posts in the table

--- components/AdminTables.vue
+++ components/AdminTables.vue
@@ -13,10 +13,20 @@ defineProps({
     required: true,
   },
 });
+
+const selected = ref([] as Post[]);
+const openDialog = ref(false);
+const store = usePostsStore();
+
+function triggerReleasePosts() {
+  selected.value.forEach((p: Post) => store.releasePost(p.id));
+}
+
 </script>
 
 <template>
   <q-table
+    v-model:selected="selected"
     bordered
     class="q-my-lg"
     :columns="adminPostColumns"
@@ -24,6 +34,7 @@ defineProps({
     flat
     :rows="posts"
     :rows-per-page-options="[5, 10,0]"
+    selection="multiple"
     title="Alle Posts"
     :wrap-cells="true"
   />
@@ -46,11 +57,70 @@ defineProps({
     class="q-my-lg"
     :columns="adminPostColumns"
     dense
-
     flat
     :rows="rejectedPosts"
     :rows-per-page-options="[5, 10,0]"
     title="Abgelehnte Posts"
     :wrap-cells="true"
   />
+
+  <q-page-sticky
+    :offset="[18, 18]"
+    position="bottom-right"
+  >
+    <q-btn
+      v-if="selected.length > 0"
+      color="red"
+      icon-right="check"
+      label="Speichern & Freigeben"
+      @click="openDialog = true"
+    />
+  </q-page-sticky>
+
+  <q-dialog
+    v-model="openDialog"
+    icon="check"
+    persistent
+  >
+    <div>
+      <q-card
+        class="bg-red-6 text-white q-pa-md"
+        style="width: 400px"
+      >
+        <q-card-section>
+          <div class="text-h6">
+            Sind Sie sicher diese {{ selected.length }} Posts zu veröffentlichen?
+          </div>
+        </q-card-section>
+
+        <q-card-section
+          v-for="selectedPost in selected"
+          :key="selectedPost.id"
+          class="q-ma-xs q-pa-xs"
+        >
+          - "{{ selectedPost.title }}"" von "{{ selectedPost.name }}"
+        </q-card-section>
+
+        <q-card-actions
+          align="right"
+          class="bg-black text-teal"
+        >
+          <q-btn
+            v-close-popup
+            color="blue"
+            flat
+            label="Abbrechen"
+          />
+          <q-btn
+            v-close-popup
+            color="red"
+            flat
+            icon-right="check"
+            label="Freigeben"
+            @click="triggerReleasePosts"
+          />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-dialog>
 </template>
