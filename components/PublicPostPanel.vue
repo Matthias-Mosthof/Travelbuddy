714793bca32f70ff35019b412e7b8c1788fa9b85
matthias <matthias.mosthof@gmail.com>
2023-12-21 23:27:21 +0100

    basic filter structure

--- components/PublicPostPanel.vue
+++ components/PublicPostPanel.vue
@@ -6,13 +6,82 @@ defineProps({
     required: true,
   },
 });
+const gender = ref('mä');
+const rangeLabel = ref({ min: 18, max: 99 });
 
+function triggerFilter() {
+  console.log(rangeLabel.value);
+}
 </script>
 
 <template>
   <div
-    class="q-pa-md"
+    class="q-pa-md  justify-content column  items-center"
   >
+    <h1
+      id="heading"
+    >
+      Postübersicht
+    </h1>
+    <q-form
+      class="q-pa-md q-ma-md fit row wrap justify-around filter-bar items-center"
+      style="max-width: 45rem;"
+    >
+      <SearchInput placeholder="Dein Suchbegriff.." />
+
+      <div class="column ">
+        <div class="row items-center align-center">
+          <div class="q-pr-md ">
+            Alter von/bis:
+          </div>
+
+          <q-range
+            v-model="rangeLabel"
+            class="q-pl-md"
+            color="primary"
+            label-always
+            markers
+            :max="99"
+            :min="18"
+            :step="1"
+            style="width:120px"
+            @change="triggerFilter"
+          />
+        </div>
+
+        <div class="row items-center">
+          <div class="q-pr-sm">
+            Geschlecht:
+          </div>
+          <q-radio
+            v-model="gender"
+            checked-icon="male"
+            label="M"
+            size="sm"
+            unchecked-icon="panorama_fish_eye"
+            val="m"
+          />
+          <q-radio
+            v-model="gender"
+            checked-icon="female"
+            label="W"
+            size="xs"
+            unchecked-icon="panorama_fish_eye"
+            val="w"
+          />
+
+          <q-radio
+            v-model="gender"
+            checked-icon="task_alt"
+            label="egal"
+            size="xs"
+            unchecked-icon="panorama_fish_eye"
+            val="egal"
+          />
+        </div>
+      </div>
+    </q-form>
+
     <TransitionGroup
       appear
       class="column q-gutter-md q-mx-xl items-center"
@@ -20,12 +89,6 @@ defineProps({
       enter-leave-class="animated fadeOut"
       tag="div"
     >
-      <h1
-        id="heading"
-        key="1"
-      >
-        Postübersicht
-      </h1>
       <PublicSceletonPosts
         v-if="posts.length < 1"
         key="2"
@@ -79,4 +142,8 @@ defineProps({
     width: 50rem;
 }
 
+.filter-bar {
+  border: 1px solid rgb(150, 150, 150);
+  border-radius: 5px;
+}
 </style>
