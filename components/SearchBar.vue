168c8744c46775ccbcbe988ba50ca05c7c9b3cc7
matthias <matthias.mosthof@gmail.com>
2023-12-22 23:09:25 +0100

    puts SearchBar into separate component

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -7,45 +7,11 @@
 
     <div class="column ">
       <div class="row items-center align-center">
-        <div class="q-pr-md ">
-          Alter von/bis:
-        </div>
         <SearchRange />
+      </div>
+      <div class="row items-center">
+        <SearchRadio />
       </div>
     </div>
   </q-form>
-
-  <div class="row items-center">
-    <div class="q-pr-sm">
-      Geschlecht:
-    </div>
-    <q-radio
-      v-model="gender"
-      checked-icon="male"
-      label="M"
-      size="sm"
-      unchecked-icon="panorama_fish_eye"
-      val="männlich"
-      @click="triggerGenderFilter"
-    />
-    <q-radio
-      v-model="gender"
-      checked-icon="female"
-      label="W"
-      size="xs"
-      unchecked-icon="panorama_fish_eye"
-      val="weiblich"
-      @click="triggerGenderFilter"
-    />
-
-    <q-radio
-      v-model="gender"
-      checked-icon="task_alt"
-      label="egal"
-      size="xs"
-      unchecked-icon="panorama_fish_eye"
-      val=""
-      @click="triggerGenderFilter"
-    />
-  </div>
 </template>
