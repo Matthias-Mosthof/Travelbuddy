010255b3d1cdc11dd2fd2b1bd764f4a789e7dee4
matthias <matthias.mosthof@gmail.com>
2023-12-22 23:04:35 +0100

    puts searchrange in separate component

--- components/PublicPostPanel.vue
+++ components/PublicPostPanel.vue
@@ -60,40 +60,6 @@ function triggerGenderFilter() {
             @change="triggerFilter"
           />
         </div>
-
-        <div class="row items-center">
-          <div class="q-pr-sm">
-            Geschlecht:
-          </div>
-          <q-radio
-            v-model="gender"
-            checked-icon="male"
-            label="M"
-            size="sm"
-            unchecked-icon="panorama_fish_eye"
-            val="männlich"
-            @click="triggerGenderFilter"
-          />
-          <q-radio
-            v-model="gender"
-            checked-icon="female"
-            label="W"
-            size="xs"
-            unchecked-icon="panorama_fish_eye"
-            val="weiblich"
-            @click="triggerGenderFilter"
-          />
-
-          <q-radio
-            v-model="gender"
-            checked-icon="task_alt"
-            label="egal"
-            size="xs"
-            unchecked-icon="panorama_fish_eye"
-            val=""
-            @click="triggerGenderFilter"
-          />
-        </div>
       </div>
     </q-form>
 
---
+++ components/SearchBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-form
+    class="q-pa-md q-ma-md fit row wrap justify-around filter-bar items-center"
+    style="max-width: 45rem;"
+  >
+    <SearchInput placeholder="Dein Suchbegriff.." />
+
+    <div class="column ">
+      <div class="row items-center align-center">
+        <div class="q-pr-md ">
+          Alter von/bis:
+        </div>
+        <SearchRange />
+      </div>
+    </div>
+  </q-form>
+
+  <div class="row items-center">
+    <div class="q-pr-sm">
+      Geschlecht:
+    </div>
+    <q-radio
+      v-model="gender"
+      checked-icon="male"
+      label="M"
+      size="sm"
+      unchecked-icon="panorama_fish_eye"
+      val="männlich"
+      @click="triggerGenderFilter"
+    />
+    <q-radio
+      v-model="gender"
+      checked-icon="female"
+      label="W"
+      size="xs"
+      unchecked-icon="panorama_fish_eye"
+      val="weiblich"
+      @click="triggerGenderFilter"
+    />
+
+    <q-radio
+      v-model="gender"
+      checked-icon="task_alt"
+      label="egal"
+      size="xs"
+      unchecked-icon="panorama_fish_eye"
+      val=""
+      @click="triggerGenderFilter"
+    />
+  </div>
+</template>
