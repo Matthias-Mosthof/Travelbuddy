a1f7d42e437571389450bd7926957dc9c0d34467
matthias <matthias.mosthof@gmail.com>
2024-01-06 22:35:23 +0100

    removes clientside filter functions

--- components/GeneralPostPanel.vue
+++ components/GeneralPostPanel.vue
@@ -11,21 +11,12 @@ const store = usePostsStore();
 
 const posts = computed(() => store.getPosts);
 
-const filter = computed(() => store.getFilter);
-
-const filteredPosts = computed((): Post[] => posts.value.filter((post: Post) => (
-  post.text?.toLowerCase().includes(filter.value.searchTerm.toLowerCase())
-  && post.title?.toLowerCase().includes(filter.value.searchTerm.toLowerCase())
-  && (post.age >= filter.value.ageRange.min && post.age <= filter.value.ageRange.max)
-  && (post.gender.includes(filter.value.gender))
-)));
-
 </script>
 
 <template>
   <PublicPostPanel
     v-if="!isAdmin"
-    :posts="filteredPosts"
+    :posts="posts"
   />
 
   <AdminPostPanel
