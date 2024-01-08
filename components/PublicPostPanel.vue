3e8102c00ae5734330e77f40a7ebf1e4bb0b8ea4
matthias <matthias.mosthof@gmail.com>
2024-01-08 23:17:40 +0100

    puts public posts into separate component

--- components/PublicPostPanel.vue
+++ components/PublicPostPanel.vue
@@ -20,8 +20,13 @@ const filterIsActive = computed(() => store.getAnyFilterIsActive);
     >
       Postübersicht
     </h1>
-
-    <SearchOverview class="q-mb-xl" />
+    <SearchOverview />
+    <q-btn
+      icon="explore"
+      label="Eigene Anzeige aufgeben"
+      outlined
+      @click="async() => await navigateTo('post-create')"
+    />
 
     <p v-if="posts.length < 1 && !filterIsActive">
       Du siehst keine Daten, weil du kein Zugriff auf die Datenbank hast.
@@ -36,50 +41,11 @@ const filterIsActive = computed(() => store.getAnyFilterIsActive);
       v-if="posts.length < 1 && !filterIsActive"
       class="one-post"
     />
-
-    <TransitionGroup
-      appear
-      class="column q-gutter-md q-mx-xl items-center"
-
-      tag="div"
-    >
-      <q-card
-        v-for="post in posts"
-        :key="post.id"
-        bordered
-        class="q-pa-md one-post status-released"
-        dark
-        flat
-      >
-        <PostContent
-          :post-age="post.age"
-          :post-name="post.name"
-          :post-text="post.text"
-          :post-title="post.title"
-        />
-
-        <PublicMessageForm
-          :email="post.email"
-          :name="post.name"
-        />
-      </q-card>
-      <PublicPostPagination key="5" />
-    </TransitionGroup>
+    <PublicPosts :posts="posts" />
   </div>
 </template>
 
-<style scoped>
-
-.post-container {
-  max-width: 100em;
-}
-.status-released {
-    background: radial-gradient(ellipse at bottom left, #003f15e3 0%, #011300 100%);
-}
-.one-post {
-    width: 50rem;
-}
-
+<style scoped lang="scss">
 .filter-bar {
   border: 1px solid rgb(150, 150, 150);
   border-radius: 5px;
---
+++ components/PublicPosts.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+defineProps({
+  posts: {
+    type: Array as () => Post[],
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <TransitionGroup
+    appear
+    class="column q-gutter-md q-ma-lg items-center"
+    tag="div"
+  >
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      bordered
+      class="q-pa-md one-post status-released"
+      dark
+      flat
+    >
+      <PostContent
+        :post-age="post.age"
+        :post-name="post.name"
+        :post-text="post.text"
+        :post-title="post.title"
+      />
+
+      <PublicMessageForm
+        :email="post.email"
+        :name="post.name"
+      />
+    </q-card>
+    <PublicPostPagination key="5" />
+  </TransitionGroup>
+</template>
+
+<style scoped>
+
+.post-container {
+  max-width: 100em;
+}
+.status-released {
+    background: radial-gradient(ellipse at bottom left, #003f15e3 0%, #011300 100%);
+}
+.one-post {
+    width: 50rem;
+}
+
+.filter-bar {
+  border: 1px solid rgb(150, 150, 150);
+  border-radius: 5px;
+}
+</style>
